2048c873d32f279e8848758ca6aa0727432a0e66
John Rogers <johnrogers@Johns-MacBook-Pro.local>
2014-04-06 09:50:05 -0700

    update

--- MCTest/FirstViewController.m
+++ MCTest/FirstViewController.m
@@ -9,9 +9,6 @@
 //  FirstViewController.m
 //  MCDemo
 //
-//  Created by Gabriel Theodoropoulos on 1/6/14.
-//  Copyright (c) 2014 Appcoda. All rights reserved.
-//
 
 #import "FirstViewController.h"
 #import "AppDelegate.h"
