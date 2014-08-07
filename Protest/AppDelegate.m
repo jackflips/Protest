6121c90e128e987eaa060c2fbd538b54beb4be7c
jackflips <johnrogers@Johns-MacBook-Air-2.local>
2014-08-06 17:34:35 -0700

    app disconnects from all peers on quit

--- Protest/AppDelegate.m
+++ Protest/AppDelegate.m
@@ -49,6 +49,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
+    [_manager disconnectFromPeers];
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
